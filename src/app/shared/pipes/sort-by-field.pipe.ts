12dbc64039572cc89d24f6cbbd4261c0f33b30ef
Kostya Struga <kennny007@yandex.ru>
2022-07-18 15:20:40 +0300

    Fix analytics page

--- src/app/shared/pipes/sort-by-field.pipe.ts
+++ src/app/shared/pipes/sort-by-field.pipe.ts
@@ -19,7 +19,7 @@ export class ArraySortPipe implements PipeTransform {
     }
 
     deepValue(obj, path) {
-        for (var i = 0, path = path.split('.'), len = path.length; i < len; i++) {
+        for (let i = 0, path = path.split('.'), len = path.length; i < len; i++) {
             obj = obj[path[i]];
         }
         return obj;
