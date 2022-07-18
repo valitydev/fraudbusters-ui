09fd23602203dd663bec2dba32afc5d31c26018c
Kostya Struga <kennny007@yandex.ru>
2022-07-18 17:48:58 +0300

    Add more spiner

--- src/app/shared/components/fb-info-card/fb-info-card.module.ts
+++ src/app/shared/components/fb-info-card/fb-info-card.module.ts
@@ -2,9 +2,9 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FlexModule } from '@angular/flex-layout';
 import { MatCardModule } from '@angular/material/card';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
 import { FbInfoCardComponent } from './fb-info-card.component';
-import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
 @NgModule({
     declarations: [FbInfoCardComponent],
