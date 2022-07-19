fd721a6e07e4e6a607a45ff7235aa282ae66784b
Kostya Struga <kennny007@yandex.ru>
2022-07-19 13:47:18 +0300

    Change roles

--- src/app/shared/pipes/format-number.pipe.ts
+++ src/app/shared/pipes/format-number.pipe.ts
@@ -2,7 +2,7 @@ import { Pipe, PipeTransform } from '@angular/core';
 
 @Pipe({ name: 'formatNumber' })
 export class FormatNumberPipe implements PipeTransform {
-    transform(num: any): number {
+    transform(num: any): any {
         return num !== 'NaN' && !!num ? num : 0.0;
     }
 }
