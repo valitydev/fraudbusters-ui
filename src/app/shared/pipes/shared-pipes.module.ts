be459ccf927d63bb39a2ff4c3dfa14fcd49892e1
Kostya Struga <kennny007@yandex.ru>
2022-08-23 12:21:34 +0300

    Fix some styles

--- src/app/shared/pipes/shared-pipes.module.ts
+++ src/app/shared/pipes/shared-pipes.module.ts
@@ -1,12 +1,19 @@
 import { NgModule } from '@angular/core';
 
 import { FormatNumberPipe } from './format-number.pipe';
+import { InspectResultToColorPipe } from './inspect-result-to-color.pipe';
 import { PaymentStatusToColorPipe } from './payment-status-to-color.pipe';
 import { ArraySortPipe } from './sort-by-field.pipe';
 import { TemplateToRulesPipe } from './template-to-rules.pipe';
 
 @NgModule({
-    declarations: [TemplateToRulesPipe, PaymentStatusToColorPipe, ArraySortPipe, FormatNumberPipe],
-    exports: [TemplateToRulesPipe, PaymentStatusToColorPipe, ArraySortPipe, FormatNumberPipe],
+    declarations: [
+        TemplateToRulesPipe,
+        PaymentStatusToColorPipe,
+        ArraySortPipe,
+        FormatNumberPipe,
+        InspectResultToColorPipe,
+    ],
+    exports: [TemplateToRulesPipe, PaymentStatusToColorPipe, ArraySortPipe, FormatNumberPipe, InspectResultToColorPipe],
 })
 export class SharedPipesModule {}
