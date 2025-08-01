9136874b1de2a2ab393f288f395e31c2e8b5970c
kostyastruga <koztyastruga@gmail.com>
2025-08-01 12:46:57 +0300

    Fix linter

--- src/app/shared/operators/boolean-delay.ts
+++ src/app/shared/operators/boolean-delay.ts
@@ -4,38 +4,36 @@ const emitWithDelay = (ms: number, observer: Subscriber<boolean>): Subscription 
     timer(ms).subscribe(() => observer.next(true));
 
 export const booleanDelay =
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-
-        (ms: number = 500, emitTrigger: Observable<any> = of(true)) =>
-        <T>(source: Observable<T>) =>
-            new Observable<boolean>((observer) => {
-                let emitterSub = Subscription.EMPTY;
-                const triggerSub = emitTrigger.subscribe(() => {
-                    emitterSub.unsubscribe();
-                    emitterSub = emitWithDelay(ms, observer);
-                });
-                const sourceSub = source.subscribe({
-                    next() {
-                        emitterSub.unsubscribe();
-                        observer.next(false);
-                    },
-                    error(err) {
-                        triggerSub.unsubscribe();
-                        emitterSub.unsubscribe();
-                        observer.next(false);
-                        observer.error(err);
-                    },
-                    complete() {
-                        triggerSub.unsubscribe();
-                        emitterSub.unsubscribe();
-                        observer.complete();
-                    },
-                });
-                return {
-                    unsubscribe() {
-                        triggerSub.unsubscribe();
-                        emitterSub.unsubscribe();
-                        sourceSub.unsubscribe();
-                    },
-                };
+    (ms: number = 500, emitTrigger: Observable<unknown> = of(true)) =>
+    <T>(source: Observable<T>) =>
+        new Observable<boolean>((observer) => {
+            let emitterSub = Subscription.EMPTY;
+            const triggerSub = emitTrigger.subscribe(() => {
+                emitterSub.unsubscribe();
+                emitterSub = emitWithDelay(ms, observer);
             });
+            const sourceSub = source.subscribe({
+                next() {
+                    emitterSub.unsubscribe();
+                    observer.next(false);
+                },
+                error(err) {
+                    triggerSub.unsubscribe();
+                    emitterSub.unsubscribe();
+                    observer.next(false);
+                    observer.error(err);
+                },
+                complete() {
+                    triggerSub.unsubscribe();
+                    emitterSub.unsubscribe();
+                    observer.complete();
+                },
+            });
+            return {
+                unsubscribe() {
+                    triggerSub.unsubscribe();
+                    emitterSub.unsubscribe();
+                    sourceSub.unsubscribe();
+                },
+            };
+        });
