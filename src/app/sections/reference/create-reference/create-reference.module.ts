24b6a5fdb67e95b9aea3605760387bb74858d2af
kostyastruga <koztyastruga@gmail.com>
2026-05-25 17:38:18 +0300

    Add tooltip

--- src/app/sections/reference/create-reference/create-reference.module.ts
+++ src/app/sections/reference/create-reference/create-reference.module.ts
@@ -9,6 +9,7 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { MatProgressBarModule } from '@angular/material/progress-bar';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatTooltipModule } from '@angular/material/tooltip';
 
 import { PaymentReferencesModule, PaymentReferencesService } from '../../../api/payments/references';
 import { HeadlineModule } from '../../../shared/components/headline';
@@ -35,6 +36,7 @@ import { CreateReferenceComponent } from './create-reference.component';
         MatIconModule,
         MatSnackBarModule,
         MatProgressBarModule,
+        MatTooltipModule,
         HeadlineModule,
     ],
     exports: [CreateReferenceComponent],
